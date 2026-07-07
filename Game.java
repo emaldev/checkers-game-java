2fd877f9349c483f4e69c61f247b08bcbc33d06b
emaldev <261611436+emaldev@users.noreply.github.com>
2026-07-07 09:09:07 +0330

    Board-level press-fit preparation

--- Game.java
+++ Game.java
@@ -21,31 +21,39 @@ public class Game {
     }
     
     public void start() {
-        Scanner input = new Scanner(System.in);
-       
-        board.displayBoard();
-        System.out.print("Enter from row: ");
-        int fromRow = input.nextInt();
-        System.out.print("Enter from col: ");
-        int fromCol = input.nextInt();
-        System.out.print("Enter to row: ");
-        int toRow = input.nextInt();
-        System.out.print("Enter to col: ");
-        int toCol = input.nextInt();
-        
-        if(board.movePiece(fromRow, fromCol, toRow, toCol)){
-            System.out.println("Move successful!");
-        }else{
-            System.out.println("Invalid move.");
-        }
+    Scanner input = new Scanner(System.in);
 
-        board.displayBoard();
-         switchTurn();
-        board.movePiece(2, 1, 3, 2);
-        System.out.println("After move: ");
-        board.displayBoard();
-        System.out.println("Current player: " + currentPlayer.getName() + "(" + currentPlayer.getColor() + ")");
-        input.close();
+    board.displayBoard();
+
+    System.out.print("Enter from row: ");
+    int fromRow = input.nextInt();
+    System.out.print("Enter from col: ");
+    int fromCol = input.nextInt();
+    System.out.print("Enter to row: ");
+    int toRow = input.nextInt();
+    System.out.print("Enter to col: ");
+    int toCol = input.nextInt();
+
+    System.out.print("Is this a jump? (yes/no): ");
+    String jump = input.next();
+
+    boolean success;
+    if (jump.equalsIgnoreCase("yes")) {
+        success = board.jumpPiece(fromRow, fromCol, toRow, toCol);
+    } else {
+        success = board.movePiece(fromRow, fromCol, toRow, toCol);
+    }
+
+    if (success) {
+        System.out.println("Move successful!");
+    } else {
+        System.out.println("Invalid move.");
+    }
+
+    board.displayBoard();
+    switchTurn();
+    System.out.println("Current player: " + currentPlayer.getName() + " (" + currentPlayer.getColor() + ")");
+    input.close();
     }
     
 }

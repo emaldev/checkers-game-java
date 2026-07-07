d912b5f4c0ab019d3472d0d01fcd41d8025ab692
emaldev <261611436+emaldev@users.noreply.github.com>
2026-07-07 08:44:32 +0330

    Adding manual input from the user

--- Game.java
+++ Game.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 public class Game {
     private Board board;
     private Player[] players;
@@ -20,12 +21,31 @@ public class Game {
     }
     
     public void start() {
-        switchTurn();
+        Scanner input = new Scanner(System.in);
+       
         board.displayBoard();
+        System.out.print("Enter from row: ");
+        int fromRow = input.nextInt();
+        System.out.print("Enter from col: ");
+        int fromCol = input.nextInt();
+        System.out.print("Enter to row: ");
+        int toRow = input.nextInt();
+        System.out.print("Enter to col: ");
+        int toCol = input.nextInt();
+        
+        if(board.movePiece(fromRow, fromCol, toRow, toCol)){
+            System.out.println("Move successful!");
+        }else{
+            System.out.println("Invalid move.");
+        }
+
+        board.displayBoard();
+         switchTurn();
         board.movePiece(2, 1, 3, 2);
         System.out.println("After move: ");
         board.displayBoard();
         System.out.println("Current player: " + currentPlayer.getName() + "(" + currentPlayer.getColor() + ")");
+        input.close();
     }
     
 }
